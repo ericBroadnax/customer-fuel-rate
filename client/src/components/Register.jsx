74cf09e682f04eab58baf5fd19eff9f8dc9d7a39
ericBroadnax <58744377+ericBroadnax@users.noreply.github.com>
2023-03-20 14:33:56 -0500

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useSate } from 'react';
 import { NavLink } from 'react-router-dom';
 import NavbarLogin from './NavbarLogin';
 import Footer from './Footer';
+import { useNavigate }  from 'react-router-dom';
 
-export default function Register() {
+export default function Register () {
+    const history = useNavigate();
+    const [user, setUser] = useState({
+        username : "",
+        email : "",
+        password : ""
+    });
+
+    //handle input
+    const handleInput = (event) =>{
+        let name = event.target.name;
+        let value = event.target.value;
+        setUser({...user, [name]:value});
+    }
+    //handle submit
+    const handleSubmit = async (event)=>{
+        event.preventDefault();
+        const {username, email,password} = user;
+        try{
+            const res = await fetch('/register', {
+                method : "POST",
+                headers : {"Content-Type" : "application/json"},
+                body : JSON.stringify({
+                    username, email, password 
+                }) 
+        });
+
+        if(res.status === 400 || !res){
+            window.alert("Already used deatils");
+        }else{
+            window.alert("Registered successfully!");
+            useNavigate.pushState('/login');
+        }
+        }catch(error){
+            console.log(error);
+        }
+    }
+    
+
     return (
         <div className="register-background">
             <NavbarLogin/>
@@ -16,16 +55,22 @@ export default function Register() {
                                 <div className="card-body text-white">
                                     <h2>Registration</h2>
 
-                                    <form action="/register-complete">
+                                    <form action="/register-complete" onSubmit={handleSubmit} method="POST">
                                         <div className="register-input-box">
                                             <i className="fa fa-envelope"></i>
-                                            <input type="text" id="userLogin" placeholder=" " minlength="2" required/>
+                                            <input type="text" id="userLogin" placeholder=" " minlength="2" required name = "username" value = {user.username} onChange = {handleInput}/>
                                             <label>Username</label>
                                         </div>
 
                                         <div className="register-input-box">
+                                            <i className="fa fa-envelope"></i>
+                                            <input type="text" id="userLogin" placeholder=" " minlength="2" required name = "email" value = {user.email} onChange = {handleInput}/>
+                                            <label>Email</label>
+                                        </div>
+
+                                        <div className="register-input-box">
                                             <i className="fa fa-lock"></i>
-                                            <input type="password" id="passLogin" placeholder=" " minlength="2" required/>
+                                            <input type="password" id="passLogin" placeholder=" " minlength="2" required name = "password" value = {user.password} onChange = {handleInput}/>
                                             <label>Password</label>
                                         </div>
 
