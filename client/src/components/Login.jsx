3a1d7701079de7d2382072df3d426d0ed09ef000
ericBroadnax <58744377+ericBroadnax@users.noreply.github.com>
2023-03-20 14:34:51 -0500

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { NavLink } from 'react-router-dom';
 import NavbarLogin from './NavbarLogin';
 import Footer from './Footer';
 
+
 export default function Login() {
+
+    /*const [user, setUser] = useState({
+        user : '',
+        password :''
+    });
+
+    const handleChange = (event) =>{
+        let name = event.target.name
+        let value = event.target.value
+
+        setUser({...user, [name]:value})
+
+    }
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+        const {username, password} = user;
+
+        try{
+            const res = await fetch('/login', {
+                method : "POST",
+                headers : {"Content-Type" : "application/json"},
+                body : JSON.stringify({
+                    username, password 
+                }) 
+        });
+
+        if(res.status === 400 || !res){
+            window.alert("Invalid credentials");
+        }else{
+            window.alert("Login success!");
+            window.location.reload();
+        }
+        }catch(error){
+            console.log(error);
+        }
+    }*/
     return (
         <div className="login-background">
             <NavbarLogin/>
@@ -19,13 +57,13 @@ export default function Login() {
                                     <form action="/home">
                                         <div className="login-input-box">
                                             <i className="fa fa-envelope"></i>
-                                            <input type="text" id="userLogin" placeholder=" " minlength="2" required/>
+                                            <input type="text" id="userLogin" placeholder=" " minlength="2" required name = "username"/>
                                             <label>Username</label>
                                         </div>
 
                                         <div className="login-input-box">
                                             <i className="fa fa-lock"></i>
-                                            <input type="password" id="passLogin" placeholder=" " required/>
+                                            <input type="password" id="passLogin" placeholder=" " required name = "password" />
                                             <label>Password</label>
                                         </div>
 
